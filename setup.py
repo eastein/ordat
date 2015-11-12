c9c3b14b6943534badb45e431563a3c7f3458442
Eric Stein <toba@des.truct.org>
2015-11-11 18:47:52 -0600

    add installability... only tested on pip install so far, no sdist or bdist test as of yet

---
+++ setup.py
@@ -0,0 +1,18 @@
+from distutils.core import setup
+
+setup(
+    name='ordat',
+    version='0.1',
+    author='Eric Stein',
+    author_email='toba@des.truct.org',
+    packages=['ordat', 'ordat.cta'],
+    package_data={
+        '': ['*.csv'],
+    },
+    url='http://github.com/eastein/ordat',
+    license='LICENSE',
+    description='Wrapper for the Chicago CTA XML API.',
+    long_description=open('README.md').read(),
+    install_requires=['requests', 'xmltodict']
+)
+
